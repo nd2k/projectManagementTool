ed0cd6b3917c2d57efb95cf1852a61029c4f2e6f
Nicolas Decat <nicolas.decat@gmail.com>
2020-06-17 22:46:43 +0200

    initial commit and application.properties

---
+++ src/main/java/com/nd2k/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.nd2k.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
